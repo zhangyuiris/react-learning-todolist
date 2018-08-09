01f6681d0f638bd0b9493eced8ef29948c0a26de
zhangyuiris <913007768zyw@gmail.com>
2018-08-10 00:03:20 +0800

    add 注释

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,22 +1,40 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 class TodoItem extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleDelete = this.handleDelete.bind(this);
-    }
-    // 子组件如果要向父组件传值，需要调用父组件传递过来的方法
+	constructor(props) {
+		super(props);
+		this.handleDelete = this.handleDelete.bind(this);
+	}
+	// 子组件如果要向父组件传值，需要调用父组件传递过来的方法
 
-    handleDelete() {
-        const { deleteItem, index} = this.props;
-        deleteItem(index)
-    }
+	handleDelete() {
+		const { deleteItem, index} = this.props;
+		deleteItem(index)
+	}
 
-    render() {
-        return (
-            <div onClick={this.handleDelete}>{this.props.content}</div>
-        )
-    }
+	render() {
+		const {content,test} = this.props;
+		return (
+			<div onClick={this.handleDelete}>
+				{test}-{content}
+			</div>
+		)
+		// return React.createElement('div', {}, React.createElement('span', {}, 'item'))
+	}
 }
+
+// 对prop接受到的东西进行强校验,此时test是没有从父组件里面传递过来的
+TodoItem.propTypes = {
+	test: PropTypes.string.isRequired,
+	content: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+	deleteItem: PropTypes.func,
+	index: PropTypes.number
+};
+
+// 默认值
+TodoItem.defaultProps = {
+	test: 'hello world'
+};
 
 export default TodoItem;
--- src/Todolist.js
+++ src/Todolist.js
@@ -1,8 +1,24 @@
 import React, { Component, Fragment} from 'react';
 import TodoItem from './TodoItem';
 
+// 如果没有import { Component, Fragment} from 'react';
+// 则需要extends React.Component
+
 class Todolist extends Component {
-  
+
+	// 仅当存在constructor的时候必须调用super，如果没有，则不用
+	// 如果在你声明的组件中存在constructor，则必须要加super
+	// 若不执行super，则this无法初始化。
+
+  // 将props传入super的作用是可以使你在constructor内访问它
+  // 	class MyClass extends React.component{
+  // 	  constructor(props){
+  // 		  super();
+  // 		  console.log(this.props); // this.props is undefined
+  // 	  }
+  // }
+
+  // 如果你只是想在别处访问它，是不必传入props的，因为React会自动为你设置
   constructor(props) {
     super(props);
     this.state = {
@@ -17,28 +33,61 @@ class Todolist extends Component {
     this.handleButtonClick = this.handleButtonClick.bind(this);
     this.deleteItem = this.deleteItem.bind(this);
   }
-  
+
+  // 使用了扩展运算符。该运算符将一个数组，变为参数序列。
+  // 例如:
+  // var arr1 = ['a', 'b'];
+	// var arr2 = ['c'];
+	// var arr3 = ['d', 'e'];
+
+  // ES5 的合并数组
+	// arr1.concat(arr2, arr3);
+
+	// ES6 的合并数组
+	// [...arr1, ...arr2, ...arr3]
+	// // [ 'a', 'b', 'c', 'd', 'e' ]
+
   handleButtonClick() {
     this.setState({
       list: [...this.state.list,this.state.inputValue],
       inputValue: ''
+    }, ()=>{
+      console.log(this.ul.querySelectorAll('div').length);
     })
-
+    // console.log(this.ul.querySelectorAll('div').length);
+    // 此时，因为setState是异步的，所以数据console length时，此时操纵dom数据还没有更新
   }
-  
-  handleInputClick(e) {
+
+
+  // 有ref之后
+	handleInputClick(e) {
+    const value = this.input.value;
     this.setState({
-      inputValue: e.target.value
+      inputValue: value
     })
   }
+  // 没有ref之前
+  // handleInputClick(e) {
+  //   // console.log(e.target);
+  //   // <input value="ssss">
+  //   this.setState({
+  //     inputValue: e.target.value
+  //   })
+  // }
 
+  // key值最好不用index
+  // a 0, b 1, c 2
+  // 因为新的渲染后
+  // b 0, c 1
+  // 用index的话没有意义
+  // 可以使用item
   getItems() {
     return (
       this.state.list.map((item,index) => {
         return (
           <TodoItem
             deleteItem={this.deleteItem}
-            key={index}
+            key={item}
             content={item}
             index={index}/>
         )
@@ -54,12 +103,16 @@ class Todolist extends Component {
     })
   }
 
+  // render里面各个元素一定要在
   render() {
     return (
       <Fragment>
-        <input value={this.state.inputValue} onChange={this.handleInputClick}/>
+        <input
+          ref={(input)=>{this.input = input}}
+          value={this.state.inputValue}
+          onChange={this.handleInputClick}/>
         <button className='btn' onClick={this.handleButtonClick}>添加</button>
-        <ul style={{color: 'blue'}}>{this.getItems()}</ul>
+        <ul style={{color: 'blue'}} ref={(ul)=>{this.ul = ul}}>{this.getItems()}</ul>
       </Fragment>
     );
   }
