c65c7d0a0af36f356b56ba3daeb90cf665dcfdce
zhangyuiris <913007768zyw@gmail.com>
2018-08-12 21:39:45 +0800

    [add]add animation

--- src/Animation.js
+++ src/Animation.js
@@ -1,4 +1,5 @@
 import React, { Component,Fragment} from 'react';
+import {CSSTransition} from 'react-transition-group'
 
 class Animation extends Component{
 
@@ -13,6 +14,16 @@ class Animation extends Component{
 	render() {
 		return (
 			<Fragment>
+				<CSSTransition
+					in={this.state.show}
+					timeout={1000}
+					classNames='fade'
+					unmountOnExit   //动画消失之后可以把这个DOM节点消除
+					onEntered={(el) => {el.style.color='blue'}}   //此钩子函数获取的el为div，当然color也可以放在CSS的fade的系列里面
+					appear={true}   //第一次出现的时候就用上该动态效果
+				>
+					<div>CSSAnimation</div>
+				</CSSTransition>
 				<div className={this.state.show ? 'show' : 'hide'}>
 					hello
 				</div>
