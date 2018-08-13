45717c2f5fc87afd97d162286790887e887a5da8
zhangyuiris <913007768zyw@gmail.com>
2018-08-13 23:46:09 +0800

    [add]add redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'antd/dist/antd.css';
 
 // 大写字母开头组件
-import Todolist from './Todolist';
-import Animation from './Animation';
-import New from './New';
+// import Todolist from './Todolist';
+// import Animation from './Animation';
+// import New from './New';
+import Addant from "./Addant";
 
-ReactDOM.render(<Todolist />, document.getElementById('root'));
-ReactDOM.render(<Animation />, document.getElementById('animation'));
-ReactDOM.render(<New />, document.getElementById('new'));
+// ReactDOM.render(<Todolist />, document.getElementById('root'));
+// ReactDOM.render(<Animation />, document.getElementById('animation'));
+// ReactDOM.render(<New />, document.getElementById('new'));
+ReactDOM.render(<Addant />, document.getElementById('antd'));
---
+++ src/store/reducer.js
@@ -0,0 +1,15 @@
+const defaultState = {
+	inputValue: '',
+	list: [
+		'Racing car sprays burning fuel into crowd.',
+		'Japanese princess to wed commoner.',
+		'Australian walks 100km after outback crash.',
+		'Man charged over missing wedding girl.',
+		'Los Angeles battles huge wildfires.',
+	]
+};
+
+// 整个数据
+export default(state = defaultState, action) => {
+	return state;
+}
